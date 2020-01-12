2f7ea2bcc0bea431db4ed6b684f70ca928ececea
NIk <nikhvatov19@gmail.com>
2020-01-12 11:22:31 +0300

    password verification

--- src/components/registor.vue
+++ src/components/registor.vue
@@ -5,8 +5,12 @@
       <img src="../assets/logo.png" alt="">
     </div>
     <div class="inputs">
+      <p class="error">{{statuslogin}}</p>
       <p> <input class="input_text" type="text" name="" id='login-inp' placeholder="Логин" value=""></p>
+      <p class="error">{{statusemail}}</p>
       <p> <input class="input_text" type="text" name="" id='email' placeholder="Электронная почта" value=""></p>
+      <p class="error">{{statuspass}}</p>
+      <p class="error">{{statuspass2}}</p>
       <p> <input class="input_text" type="password" name="" id='passw1' placeholder="Пароль" value=""></p>
       <p> <input class="input_text" type="password" name="" id='passw2' placeholder="Подтвердите пароль" value=""></p>
       <button class='btn_input' type="submit" @click="reg" name="button">Регистрация</button>
@@ -28,25 +32,62 @@ import $ from "jquery"
 
 export default {
   name: 'registor',
+  data() {
+    return {
+      statuslogin: '',
+      statusemail: '',
+      statuspass: '',
+      statuspass2: '',
+    }
+  },
   methods: {
     AuthTemplate(data) {
       this.$emit('AuthTemplate', data);
     },
     reg() {
-      console.log($("#login-inp").val())
-      console.log($('#email').val())
-      console.log($('#passw1').val())
-      console.log($('#passw2').val())
-      if ($('#passw1').val() == $('#passw2').val()) {
+      let email = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+      console.log($("#login-inp").val().length);
+      if ($('#passw1').val() == $('#passw2').val() && email.test($('#email').val()) == true && $("#login-inp").val().length > 3 && $('#passw1').val().length > 7) {
         let data = $("#login-inp").val() + ',' + $('#email').val() + ',' + $('#passw1').val();
         this.$emit('reg', data);
+        //
       }
+      if ($("#login-inp").val().length < 4) {
+        this.statuslogin = 'Слишком короткий логин'
+        //
+      } else {
+        this.statuslogin = ''
+      }
+      if (email.test($('#email').val()) == false) {
+        this.statusemail = 'Не корректный email'
+        //
+      } else {
+        this.statusemail = ''
+      }
+      if ($('#passw1').val().length < 7) {
+        this.statuspass = 'Пароль должен быть меньше 7 символов'
+        //
+      } else {
+        this.statuspass = ''
+      }
+      if ($('#passw1').val() != $('#passw2').val()) {
+        this.statuspass = 'Пароли не совподают'
+        //
+      } else {
+        this.statuspass2 = ''
+      }
+
     },
   }
 }
 </script>
 
 <style scoped>
+.error {
+  color: red;
+  font-size: 14px;
+}
+
 .registor {
   margin: 0px;
   background: #1d3e51;
